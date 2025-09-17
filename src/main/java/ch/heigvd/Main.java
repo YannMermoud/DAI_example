c9b1f937c73449b0f22ea069b3d793a88d8f0ebb
YannMermoud <yann.mermoud@heig-vd.ch>
2025-09-17 13:24:03 +0200

    Initial commit

---
+++ src/main/java/ch/heigvd/Main.java
@@ -0,0 +1,30 @@
+package ch.heigvd;
+
+import ch.heigvd.commands.Goodbye;
+import ch.heigvd.commands.Hello;
+import java.io.File;
+import picocli.CommandLine;
+
+@CommandLine.Command(description = "A small CLI with subcommands to demonstrate picocli.", version = "1.0.0", showDefaultValues = true, subcommands = {
+        Hello.class,
+        Goodbye.class,
+}, scope = CommandLine.ScopeType.INHERIT, mixinStandardHelpOptions = true)
+public class Main {
+
+    @CommandLine.Parameters(index = "0", description = "The name of the user.", defaultValue = "World")
+    protected String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public static void main(String[] args) {
+        // Source: https://stackoverflow.com/a/11159435
+        String jarFilename = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())
+                .getName();
+
+        int exitCode = new CommandLine(new Main()).setCommandName(jarFilename).execute(args);
+
+        System.exit(exitCode);
+    }
+}
---
+++ src/main/java/ch/heigvd/commands/Hello.java
@@ -0,0 +1,22 @@
+package ch.heigvd.commands;
+
+import ch.heigvd.Main;
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "hello", description = "Print a 'Hello World!' type of message.")
+public class Hello implements Callable<Integer> {
+
+    @CommandLine.ParentCommand
+    protected Main parent;
+
+    @CommandLine.Option(names = { "-g",
+            "--greetings" }, description = "The greetings to address the user.", defaultValue = "Hello")
+    protected String greetings;
+
+    @Override
+    public Integer call() {
+        System.out.println(greetings + " " + parent.getName() + "!");
+        return 0;
+    }
+}
